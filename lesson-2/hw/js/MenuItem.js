432c1ea4d16e9abcf9d71e818920c39b61d60487
gmc24 <impress@yandex.ru>
2017-12-17 18:33:34 +0300

    многоуровневость

--- lesson-2/hw/js/MenuItem.js
+++ lesson-2/hw/js/MenuItem.js
@@ -1,12 +1,12 @@
 function MenuItem(href, title, haveSub) {
     this.href = href;
     this.title = title;
-    (haveSub == undefined || haveSub == []) ? this.haveSub = [] : this.haveSub = haveSub;
+    (haveSub == undefined) ? this.haveSub = [] : this.haveSub = haveSub;
 }
 
 MenuItem.prototype.render = function () {
     // console.log(this.haveSub);
-    if (this.haveSub == []) {
+    if (this.haveSub.length == 0) {
         return '<li><a href="' + this.href + '">' + this.title + '</a></li>';
     } else {
         return '<li>' + this.title;
